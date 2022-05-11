e7bb55e52fdfe4b4ff2d101f0cca000553770550
will-da-pro <william@dolier.net>
2022-05-11 19:01:38 +1000

    Added thrust sound and collision script

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,12 +7,14 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     Rigidbody rb;
+    AudioSource audioSource;
     [SerializeField] float mainThrust = 100f;
     [SerializeField] float rotationThrust = 100f;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -27,6 +29,14 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
     }
 
