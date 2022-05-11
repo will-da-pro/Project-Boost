5c77b82cf14258b9febd2faab831fd666c5463be
will-da-pro <william@dolier.net>
2022-05-11 19:14:03 +1000

    s

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -2,8 +2,19 @@ using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
-    void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision other)
     {
-        
+        switch(other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("Friendly");
+                break;
+            case "Finish":
+                Debug.Log("Finish");
+                break;
+            default:
+                Debug.Log("Untagged");
+                break;
+        }
     }
 }
